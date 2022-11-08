1a5bfc675b81ea7eee8a9e1b3655fa74ec22fe0e
Andrew Karnaushenko <allloneneon@gmail.com>
2022-11-08 14:17:29 +0300

    rest

---
+++ test-sql/task2.sql
@@ -0,0 +1,7 @@
+SELECT ROUND(CAST(width as numeric), -1)  as width, 
+    ROUND(CAST(depth as numeric), -1) as depth,
+    ROUND(CAST(height as numeric), -1) as height,
+    COUNT(*) as count
+FROM notebooks_notebook
+GROUP BY 1, 2, 3
+ORDER BY 1
